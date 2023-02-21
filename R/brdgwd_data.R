a5ad5e772478440b1ce7558672ca0fd839f4b3b6
Raphael Saldanha <rfsaldanha@gmail.com>
2023-02-21 16:13:14 +0100

    brdwgd data.

--- R/brdgwd_data.R
+++ R/brdgwd_data.R
@@ -1,9 +1,67 @@
 brdwgd_data <- list(
-  "tmax" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Ftmax.parquet",
-  "tmin" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Ftmin.parquet",
-  "pr" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Fpr.parquet",
-  "eto" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Feto.parquet",
-  "rh" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Frh.parquet",
-  "rs" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Frs.parquet",
-  "u2" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Fu2.parquet"
+  "tmax" = list(
+    "link" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Ftmax.parquet",
+    "stats" = list(
+      "min" = "Tmax_min",
+      "max" = "Tmax_max",
+      "mean" = "Tmax_mean",
+      "sd" = "Tmax_sd"
+    )
+  ),
+  "tmin" = list(
+    "link" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Ftmin.parquet",
+    "stats" = list(
+      "min" = "Tmin_min",
+      "max" = "Tmin_max",
+      "mean" = "Tmin_mean",
+      "sd" = "Tmin_sd"
+    )
+  ),
+  "pr" = list(
+    "link" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Fpr.parquet",
+    "stats" = list(
+      "min" = "pr_min",
+      "max" = "pr_max",
+      "mean" = "pr_mean",
+      "sd" = "pr_sd",
+      "sum" = "sum_sd"
+    )
+  ),
+  "eto" = list(
+    "link" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Feto.parquet",
+    "stats" = list(
+      "min" = "ETo_min",
+      "max" = "ETo_max",
+      "mean" = "ETo_mean",
+      "sd" = "ETo_sd",
+      "sum" = "ETo_sd"
+    )
+  ),
+  "rh" = list(
+    "link" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Frh.parquet",
+    "stats" = list(
+      "min" = "RH_min",
+      "max" = "RH_max",
+      "mean" = "RH_mean",
+      "sd" = "RH_sd"
+    )
+  ),
+  "rs" = list(
+    "link" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Frs.parquet",
+    "stats" = list(
+      "min" = "Rs_min",
+      "max" = "Rs_max",
+      "mean" = "Rs_mean",
+      "sd" = "Rs_sd"
+    )
+  ),
+  "u2" = list(
+    "link" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Fu2.parquet",
+    "stats" = list(
+      "min" = "u2_min",
+      "max" = "u2_max",
+      "mean" = "u2_mean",
+      "sd" = "u2_sd"
+    )
+  )
 )
