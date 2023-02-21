a730a5d9defc90b4c171483bd5995af9d098712b
Raphael Saldanha <rfsaldanha@gmail.com>
2023-02-21 17:26:10 +0100

    Includen documentation and check date end is greather than start date.

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -1,10 +1,31 @@
+#' Fetch data from given parameters
+#'
+#' @param code_muni IBGE municipality code number with 7 digits.
+#' @param product Zonal indicator procuct. Currently, only `brdwgd`.
+#' @param indicator Indicator name from the product. Check the available indicators with the \link{product_info} function.
+#' @param statistics Statistics name from the indicator.
+#' @param date_start A date.
+#' @param date_end A date, >= than `date_start`.
+#'
+#' @return A `data.frame`.
+#' @export
+#'
+#' @examples
+#' fetch_data(
+#'     code_muni = 3304557,
+#'     product = "brdwgd",
+#'     indicator = "tmax",
+#'     statistics = "mean",
+#'     date_start = as.Date("2008-01-01"),
+#'     date_end = as.Date("2008-01-10")
+#'  )
 fetch_data <- function(code_muni, product, indicator, statistics, date_start, date_end){
   # Argument check
   checkmate::assert_number(x = code_muni)
   checkmate::assert_string(x = as.character(code_muni), n.chars = 7)
   checkmate::assert_choice(x = product, choices = "brdwgd")
   checkmate::assert_date(date_start)
-  checkmate::assert_date(date_end)
+  checkmate::assert_date(date_end, lower = date_start)
 
   # Argument check and retrieve product info
   if(product == "brdwgd"){
