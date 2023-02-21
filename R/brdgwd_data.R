c1d9946a64cbbdb180e5f1823ff610c7467e242f
Raphael Saldanha <rfsaldanha@gmail.com>
2023-02-21 15:32:35 +0100

    brdwgd data links.

---
+++ R/brdgwd_data.R
@@ -0,0 +1,9 @@
+brdwgd_data <- list(
+  "tmax" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Ftmax.parquet",
+  "tmin" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Ftmin.parquet",
+  "pr" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Fpr.parquet",
+  "eto" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Feto.parquet",
+  "rh" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Frh.parquet",
+  "rs" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Frs.parquet",
+  "u2" = "https://brdwgd.nyc3.cdn.digitaloceanspaces.com/parquet%2Fu2.parquet"
+)
